9793d34b579e6d22a0c2212f4923efdf4ef59409
Nam <phamquangnam147258@gmail.com>
2026-04-13 17:22:34 +0700

    Add bark.js file

---
+++ bark.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("Welcome to JavaScript programming.");
+console.log("This is a simple example of a JavaScript file.");
+console.log("You can run this code in a web browser or using Node.js.");
+console.log("Have fun learning JavaScript!");   
